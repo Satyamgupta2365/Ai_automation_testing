21b4c550b7521e34cc80d1adb8e009497cc447d2
SATYAM GUPTA <143061930+Satyamgupta2365@users.noreply.github.com>
2025-04-23 16:04:56 +0530

    commit

---
+++ main.py
@@ -0,0 +1,22 @@
+import undetected_chromedriver as uc # Changed the import statement
+from selenium.webdriver.common.keys import Keys
+from selenium.webdriver.common.by import By
+import time
+
+options = uc.ChromeOptions()
+driver = uc.Chrome(options=options)
+
+driver.get("https://www.google.com")
+search_box = driver.find_element(By.NAME, "q")
+
+search_box.clear()
+search_box.send_keys("Who is the PM of India")
+search_box.send_keys(Keys.RETURN)
+time.sleep(3)
+
+if "Modi" in driver.page_source:
+    print("The current Prime Minister of India is Narendra Modi.")
+else:
+    print("The current Prime Minister of India is not Narendra Modi.")
+driver.quit()
+
